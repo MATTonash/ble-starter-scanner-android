4b463fbe518fa14f6ce1841a05da3974b9232865
anyuwa0 <ayuu0018@student.monash.edu>
2025-08-29 13:30:19 +1000

    Fix connection issue, which prevented scan results from showing on the recycler.

--- app/src/main/java/com/punchthrough/blestarterappandroid/BluetoothWorkerClass.kt
+++ app/src/main/java/com/punchthrough/blestarterappandroid/BluetoothWorkerClass.kt
@@ -42,12 +42,13 @@ class BluetoothWorkerClass private constructor() {
     private val handler = Handler(Looper.getMainLooper())
 
     private val beaconProjects = mapOf(
-        "80:EC:CC:CD:33:28" to Beacon("Losing THings", -60, 0.0, 1.0),
+        "80:EC:CC:CD:33:28" to Beacon("Losing Things", -60, 0.0, 1.0),
         "80:EC:CC:CD:33:7C" to Beacon("Happy Mornings", -57, 1.0, 2.0),
         "80:EC:CC:CD:33:7E" to Beacon("STEM", -59, 2.0, 2.0),
         "80:EC:CC:CD:33:58" to Beacon("Visual Clutter", -60, 2.0, 1.0),
         "00:3C:84:28:87:01" to Beacon("MAP", -58, 1.0, 0.0),
         "00:3C:84:28:77:AB" to Beacon("Dance", -60, 1.00, 1.0),
+        "D8:F2:C8:9B:33:34" to Beacon("Origin", -62, 0.0, 0.0)
     )
 
     private lateinit var vibrator: Vibrator
@@ -283,7 +284,7 @@ class BluetoothWorkerClass private constructor() {
             }
 
             // Check and maintain connections
-            checkAndMaintainConnections()
+            //checkAndMaintainConnections()
 
             // Notify callback on main thread
             handler.post {
