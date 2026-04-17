1c8c1baf3b7e5a18b03ece0b02a641350678ae41
Connor Macdougall <connoramacdougall@gmail.com>
2026-04-17 12:28:06 +1000

    Feat: Update Kalman filter variables to be more realistic

--- app/src/main/java/com/matt/guidebeacons/beacons/Beacon.kt
+++ app/src/main/java/com/matt/guidebeacons/beacons/Beacon.kt
@@ -1,10 +1,7 @@
 package com.matt.guidebeacons.beacons
 
 import kotlinx.serialization.Serializable
-import kotlin.collections.plusAssign
-import kotlin.div
 import kotlin.math.pow
-import kotlin.times
 
 /**
  * We decided to implement a Beacon class that will store and manage all the relevant mapping data and methods
@@ -12,69 +9,67 @@ import kotlin.times
  * Previously we were relying on referencing MainActivity
  */
 @Serializable(with = BeaconSerializer::class)
-class Beacon(beaconName: String,
-             calibrationRSSI: Int,
+class Beacon(private var beaconName: String,
+             private var calibrationRSSI: Int,
              x: Double,
              y: Double,
              z: Double
 ) {
-    private var beaconName = beaconName
-    private var calibrationRSSI = calibrationRSSI
     private var coordinates = doubleArrayOf(x, y, z)
     private var buzzerSensitivity = 0
     private var beaconType = BeaconType.DEFAULT
 
     // Kalman filter variables
     private var filteredRSSI: Double = calibrationRSSI.toDouble() // Arbitrary value
-    private var estimateError: Double = 1.0  // P - estimate uncertainty
-    private val processNoise: Double = 0.01  // Q - how much we expect RSSI to change
-    private val measurementNoise: Double = 4.0  // R - sensor noise (tune based on RSSI variance)
+    private var estimateError: Double = 50.0  // P - estimate uncertainty
+    private val processNoise: Double = 0.5  // Q - how much we expect RSSI to change
+    private val measurementNoise: Double = 25.0  // R - sensor noise (tune based on RSSI variance)
     private var isInitialized: Boolean = false
 
-    public fun calculateDistance(rssi: Int, txPower: Int): Double{
+    fun calculateDistance(rssi: Int, txPower: Int): Double{
         return 10.0.pow((calibrationRSSI - rssi).toDouble()/(10*txPower).toDouble())
     }
 
-    public fun getCalibrationRSSI(): Int {
+    fun getCalibrationRSSI(): Int {
         return calibrationRSSI
     }
 
-    public fun getCoordinates(): DoubleArray {
+    fun getCoordinates(): DoubleArray {
         return coordinates
     }
 
-    public fun getCoordinatesString(): String {
+    fun getCoordinatesString(): String {
         // bit gross to hard-code indices, is there a better way?
         return "(${coordinates[0]}, ${coordinates[1]}, ${coordinates[2]})"
     }
 
-    public fun getBuzzerSensitivity(): Int {
+    fun getBuzzerSensitivity(): Int {
         return buzzerSensitivity
     }
 
-    public fun getBeaconType(): BeaconType {
+    fun getBeaconType(): BeaconType {
         return beaconType
     }
 
-    public fun setBuzzerSensitivity(sensitivity: Int) {
+    fun setBuzzerSensitivity(sensitivity: Int) {
         buzzerSensitivity = sensitivity
     }
 
-    public fun setBeaconType(type: BeaconType) {
+    fun setBeaconType(type: BeaconType) {
         beaconType = type
     }
 
-    public override fun toString(): String {
-        return beaconName;
+    override fun toString(): String {
+        return beaconName
     }
 
-    public fun updateData(beaconName: String, calibrationRSSI: Int, x: Double, y: Double, z: Double) {
+    fun updateData(beaconName: String, calibrationRSSI: Int, x: Double, y: Double, z: Double) {
         this.beaconName = beaconName
         this.calibrationRSSI = calibrationRSSI
         this.coordinates = doubleArrayOf(x, y, z)
     }
 
-    public fun updateFilteredRSSI(measuredRSSI: Int): Double {
+    fun updateFilteredRSSI(measuredRSSI: Int): Double {
         if (!isInitialized) {
             filteredRSSI = measuredRSSI.toDouble()
             isInitialized = true
@@ -92,11 +87,11 @@ class Beacon(beaconName: String,
         return filteredRSSI
     }
 
-    public fun getFilteredRSSI(): Double {
+    fun getFilteredRSSI(): Double {
         return filteredRSSI
     }
 
-    public fun resetKalmanFilter() {
+    fun resetKalmanFilter() {
         this.filteredRSSI = calibrationRSSI.toDouble()
         this.isInitialized = false
     }
